b3306f39f2dabe678b34c7555652641ccc1b81e6
dunnoconnor <moconnor8@gmail.com>
2021-12-22 13:29:11 -0500

    revised db for dev

--- db.js
+++ db.js
@@ -1,58 +1,22 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const path = require('path');
 
-let sequelize;
-if (process.env.NODE_ENV === "development") {
-  sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "bin/db.sqlite",
-    logging: false,
-    define: {
-      underscored: true,
-      underscoredAll: true,
-      freezeTableName: true,
-      timestamps: false,
-    },
-  });
-}
-if (process.env.NODE_ENV === "test") {
-  sequelize = new Sequelize({
-    dialect: "sqlite:memory:",
-    logging: false,
-    define: {
-      underscored: true,
-      underscoredAll: true,
-      freezeTableName: true,
-      timestamps: false,
-    },
-  });
-}
+// comment out development sequelize
+// const sequelize = new Sequelize({
+//     dialect: 'sqlite',
+//     storage: path.join(__dirname, 'db.sqlite')
+// })
 
-if (process.env.NODE_ENV === "production") {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    logging: false,
-    define: {
-      underscored: true,
-      underscoredAll: true,
-      freezeTableName: true,
-      timestamps: false,
-    },
-  });
-
-  sequelize
-    .authenticate()
-    .then(() => {
-      console.log("Connection has been established successfully.");
-    })
-    .catch((err) => {
-      console.error("Unable to connect to the database:", err);
-    });
+const connectionSettings = {
+    test: {dialect: 'sqlite', storage: 'sqlite::memory:'},
+    dev: {dialect: 'sqlite', storage: path.join(__dirname, 'db.sqlite')},
+    production: {dialect: 'postgres', protocal: 'postgres'}
 }
+const sequelize = process.env.NODE_ENV === 'production'
+    ? new Sequelize(
+        process.env.DATABASE_URL,
+        connectionSettings[process.env.NODE_ENV],
+        )
+    : new Sequelize(connectionSettings[process.env.NODE_ENV])
 
 module.exports = {sequelize}
